96b9a90f9ebf8a2e9306fb8e818785232a5f72cb
92sdg1 <s.grant29@yahoo.com>
2026-05-26 19:35:16 -0500

    Delete terraform/1-backend.tf

--- terraform/1-backend.tf
+++
@@ -1,9 +0,0 @@
-#establishes the backend for this project including the bucket with the file type that will occupy the bucket
-
-
-terraform {
-    backend "gcs" {
-        bucket = "wk-10"
-        prefix = "terraform/state"
-    }
-}
